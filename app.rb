d1d16dad0a2ee536e00d246aaf2f68c9ad243395
Natali <rinykia@gmail.com>
2015-09-11 15:48:46 +0300

    Add Rakefile

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'sinatra/activerecord'
+
+set :datebase, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+
+end
 
 get '/' do 
   erb "Hello"
