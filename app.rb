adeb69bd258115a038aec8180c4535ce67e0b56c
rinykia <rinykia@gmail.com>
2015-09-12 21:29:08 +0300

    Keep values on post

--- app.rb
+++ app.rb
@@ -24,16 +24,17 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
   erb :visit
 end
 
 post '/visit' do
 
-  c = Client.new params[:client]
-  if c.save
+  @c = Client.new params[:client]
+  if @c.save
     erb "Tnks!"
   else
-  	@error = c.errors.full_messages.first
+  	@error = @c.errors.full_messages.first
   	erb :visit
   end
 end
